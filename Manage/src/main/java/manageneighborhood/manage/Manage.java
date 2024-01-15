001f5a2345ed7c8e8d1f77698c6846c3dabb4426
Tran-Mai-Anh <155942841+Tran-Mai-Anh@users.noreply.github.com>
2024-01-15 19:17:56 +0700

    v1

---
+++ Manage/src/main/java/manageneighborhood/manage/Manage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package manageneighborhood.manage;
+
+/**
+ *
+ * @author maianhtran
+ */
+public class Manage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
